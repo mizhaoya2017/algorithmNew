4c03d53d370b770cd14c98f1be58095a629f4920
mizy@wdcloud.cc <mizy@wdcloud.cc>
2021-09-13 22:48:08 +0800

    初始化

---
+++ algorithm-new/src/main/java/com/mzy/algorithmnew/AlgorithmNewApplication.java
@@ -0,0 +1,13 @@
+package com.mzy.algorithmnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmNewApplication.class, args);
+    }
+
+}
